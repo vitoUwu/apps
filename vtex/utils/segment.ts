f6848821f4f92754de3ef3defcb102125694ae3c
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2025-11-21 12:46:31 -0400

    fix: safe parse segment

--- vtex/utils/segment.ts
+++ vtex/utils/segment.ts
@@ -113,7 +113,13 @@ const serialize = ({
   return btoa(JSON.stringify(seg));
 };
 
-const parse = (cookie: string) => JSON.parse(atob(cookie));
+const parse = (cookie: string) => {
+  try {
+    return JSON.parse(atob(cookie));
+  } catch {
+    return null;
+  }
+};
 
 const SEGMENT_QUERY_PARAMS = [
   "utmi_campaign" as const,
